f8535603daf6cd08cbf205b996ec402b36511346
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2023-11-15 06:52:07 -0600

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+h
+8app/src/main/java/com/yambrosio/accountapp/AppAccount.kt,4\3\432c20761b0f8835fa697d90eb9474a04053a52a
---
+++ app/src/main/java/com/yambrosio/accountapp/AppAccount.kt
@@ -0,0 +1,7 @@
+package com.yambrosio.accountapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppAccount : Application()
